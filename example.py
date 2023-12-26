f6008e782a3241aba6b26b3262f85b335c2341e6
FallMaple <tony53517230@gmail.com>
2023-12-26 19:38:26 +0000

    fix: example.py 的 img 路徑處理放錯位置

--- example.py
+++ example.py
@@ -4,6 +4,6 @@ import utils
 import cv2
 
 filename = '000998_3.jpg'
-img = cv2.imread()
-translater = CAPTCHA_Translater(utils.get_img_path(filename))
+img = cv2.imread(utils.get_img_path(filename))
+translater = CAPTCHA_Translater()
 print(translater.translate(img))
